743d3575152316eaf9d2d2a741539de048dbced3
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-04-07 09:36:38 +0200

    Reafctored generator.py

--- data/static/generator.py
+++ data/static/generator.py
@@ -6,89 +6,63 @@ import requests as req
 from pathlib import Path
 from data.apis.teams_api import TeamsAPI as tapi
 
-
-api = tapi()
+# I cant import the repo class due
+# to circular imports
+team_api = tapi()
 
 class Static:
-    
     
     url = "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams"
 
     team_ids_path = "./data/static/team_ids.csv"
-    team_points_path = "./team_points"
+    team_points_path = "./data/static/team_points.csv"
+    team_percentages_path = "./data/static/win_percentages.csv"
+    last_updated_path = "last_updated.txt"
+    win_perc_path = "./data/static/win_percentages.csv"
 
     def __init__(self) -> None:
-        last_updated = self.reading_state()
-        today = str(dt.date.today())
-        if last_updated != today :
-            self.generate_team_points()
-            self.generate_team_percentages()
-        self.initialize_static_file()
+        self.generate_team_stats()
+        self.genarate_team_ids()
   
 
     def genarate_team_ids(self):
-        data = req.get(self.url).text
-
-        # Serialize the JSON and only extract the Sports Atrribute
-        data = json.loads(data)["sports"]
-
-        # Extract Leagues attribute
-        leagues = data[0]["leagues"]
-
-        # Extract the NBA
-        league = leagues[0]
-
-        # Get teams
-        teams = league["teams"]
-
-        ids = []
-        abbr = []
-        display_names = []
-            
-        # Extract every teams name, initial, short name and display name
-        for x in range(len(teams)):    
-            ids.append(int(teams[x]["team"]["id"]))
-            abbr.append(teams[x]["team"]["abbreviation"])
-            display_names.append(teams[x]["team"]["displayName"])
-
-        result = pd.DataFrame({
-            'Id':ids,
-            'Abbreviation':abbr,
-            'Display Name':display_names
-        })
-
-        results = result.sort_values('Id')
-        return results
-
-    def team_ids_file_exist(self) -> bool:
-        return path.exists(self.team_ids_path)
-
-    def write_team_ids(self, data:str):
-        with open(self.team_ids_path, 'w') as file:
-            file.write(data)
-
-    def initialize_static_file(self):
-
-        exists = self.team_ids_file_exist()
-
-        # Only if path doesn't exist
-
+        exists = path.exists(self.team_ids_path)
         if(exists == False):
-            data = self.genarate_team_ids().to_csv()
-            self.write_team_ids(data=data)
+            data = req.get(self.url).text
+            data = json.loads(data)["sports"]
+            teams = data[0]["leagues"][0]["teams"]
 
-    def read_team_ids(self):
+            ids = []
+            abbr = []
+            display_names = []
+                
+            # Extract every teams name, initial, short name and display name
+            for x in range(len(teams)):    
+                ids.append(int(teams[x]["team"]["id"]))
+                abbr.append(teams[x]["team"]["abbreviation"])
+                display_names.append(teams[x]["team"]["displayName"])
+
+            result = pd.DataFrame({
+                'Id':ids,
+                'Abbreviation':abbr,
+                'Display Name':display_names
+            })
+
+            result = result.sort_values('Id')
+            result.to_csv(self.team_ids_path)
+
+    def get_team_ids(self):
         return pd.read_csv(self.team_ids_path)
 
     def get_team_id_by_abbr(self, abbr:str) -> int:
-        df = self.read_team_ids()
+        df = self.get_team_ids()
         team = df.loc[df["Abbreviation"] == abbr]
         id = int(team["Id"])
         return id
         
     
     def get_team_id_by_name(self,display_name):
-        df = self.read_team_ids()
+        df = self.get_team_ids()
         team = df.loc[df["Display Name"] == display_name]
         return team["Id"][0]
     
@@ -103,55 +77,50 @@ class Static:
 
         return teams
 
-    def generate_team_points(self):
+    def generate_team_stats(self):
         today = str(dt.date.today())
-        last_update = self.reading_state()
+        last_update = self.read_date_last_updated()
+        team_points = []
+        team_names = []
+        percentages = []
 
         if today != last_update:
-            team_points = api.getAllTeamsPoints()
-            team_points = team_points.to_csv("./data/static/team_points.csv")
-            self.write_state(today)
+            for x in range(1, 31):
+                team = team_api.getTeamData(x)
+                team_names.append(team.display_name)
+                team_points.append(team.stats.points_scored)
+                percentages.append(team.stats.win_percentage)
+
+            percentages_df = pd.DataFrame({
+                "Team":team_names,
+                "Win Percentage":percentages
+            })
+
+            points_df = pd.DataFrame({
+                "Team":team_names,
+                "Points Scored":team_points
+            })
+
+            percentages_df.to_csv(self.team_percentages_path)
+            points_df.to_csv(self.team_points_path)
+            self.write_date_last_updated(today)
+
 
     def get_team_points(self):
-        team_points = pd.read_csv("./data/static/team_points.csv")
-        return team_points
-
-    def generate_team_percentages(self):
-    
-        percentages = []
-        team = []
-
-        for x in range(1, 31):
-            data = api.getTeamData(x)
-            team.append(data.display_name)
-            percentages.append(round(data.stats.win_percentage, 2))
-
-        df = pd.DataFrame({
-            "Team":team,
-            "Win Percentage":percentages
-        })
-
-        file = open("./data/static/win_percentages.csv", "w")
-        string = df.to_csv()
-        file.write(string)
-        file.close()
-        today = str(dt.date.today())
-        self.write_state(today)
+        self.generate_team_stats()
+        return pd.read_csv(self.team_points_path)
 
     def get_win_percentages(self):
-        state = self.reading_state()
-        today = str(dt.date.today())
-        if state != today:
-            self.generate_team_percentages()
-        return pd.read_csv("./data/static/win_percentages.csv")
+        self.generate_team_stats()
+        return pd.read_csv(self.win_perc_path)
 
-    def write_state(self, string:str):
-        file = open("state.txt", "w")
+    def write_date_last_updated(self, string:str):
+        file = open(self.last_updated_path, "w")
         file.write(string)
         file.close()
     
-    def reading_state(self) -> list[str]:
-        file = open("state.txt", "r")
-        data = file.read()
+    def read_date_last_updated(self) -> list[str]:
+        file = open(self.last_updated_path, "r")
+        date = file.read()
         file.close()
-        return data
+        return date
