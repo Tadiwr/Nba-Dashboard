6bc096096ead7a905e73594b05f7210d30959ac2
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-04-18 21:59:10 +0200

    added status module to requests file

--- data/apis/teams_api.py
+++ data/apis/teams_api.py
@@ -2,6 +2,7 @@ import requests as req
 import json
 from data.models.team_model import TeamModel
 import pandas as pd 
+from data.status import Status as sts
 
 class TeamsAPI:
 
@@ -9,6 +10,7 @@ class TeamsAPI:
 
     def getTeamData(self, id:int):
         team_json = req.get(self.url + str(id)).text
+        sts().update_req_counts()
         team_dict = json.loads(team_json)
         team_dict = team_dict["team"]
         team_model = TeamModel(team_dict)
